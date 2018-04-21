3f09c93516b256ed60edd36d5f4dfd99bf07f6e3
lab 1 PC 08 <lab 1 PC 08>
2018-04-21 16:31:40 -0500

    ff

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCZvmLp7c-fkyQoOaOxxAjmnMLmPiGiDqM",
+    authDomain: "bookstore-b6820.firebaseapp.com",
+    databaseURL: "https://bookstore-b6820.firebaseio.com",
+    projectId: "bookstore-b6820",
+    storageBucket: "bookstore-b6820.appspot.com",
+    messagingSenderId: "1017976492176"
+};
